dc3378e92433a7cc42ecce0a81181715bf062294
tienloi244 <139633455+tienloi244@users.noreply.github.com>
2023-08-05 17:10:41 +0700

    Update Priority_queue.h

--- Priority_queue.h
+++ Priority_queue.h
@@ -9,8 +9,8 @@ struct Node {
 };
 
 struct Priority_queue { // max heap
-    Node* front = nullptr;
-    Node* rear = nullptr;
+    Node* front = NULL;
+    Node* rear = NULL;
 };
 
 void swap(int& a, int& b);
@@ -22,7 +22,7 @@ Node* findNode(Priority_queue q, int i);
 void shiftUp(Priority_queue& q, int i);
 void shiftDown(Priority_queue& q, int i);
 void enqueue(Priority_queue& q, int key);
-bool is_empty(Priority_queue q);
+bool check_empty(Priority_queue q);
 int count_element(Priority_queue q);
 void remove_all(Priority_queue& q);
 void insert_element(Priority_queue& q, int key);
